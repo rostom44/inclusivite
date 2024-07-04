a01667f88120a060936e27b0e30558c995430cfa
rostom44 <blackgamerdv7@gmail.com>
2024-07-04 11:36:38 +0200

    pics_done

--- src/components/Phase2.jsx
+++ src/components/Phase2.jsx
@@ -1,5 +1,5 @@
 import { Box, Paper, Typography } from "@mui/material";
-import entretient from "../assets/entretient.jpeg";
+import communication from "../assets/communication.jpg";
 
 export default function Phase2() {
   return (
@@ -44,7 +44,7 @@ export default function Phase2() {
         </Box>
         <Box
           component="img"
-          src={entretient}
+          src={communication}
           alt="Entretient image"
           sx={{
             width: "45%",
