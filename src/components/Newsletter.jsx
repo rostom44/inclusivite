22a2ca8e59d953213673c64ce29b7d355ac89cd6
YohanGH <YohanGH@proton.me>
2024-07-04 15:02:46 +0200

    add content in NewLetter and create media query for mui
    Signed-off-by: YohanGH <YohanGH@proton.me>

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,68 +1,85 @@
 import {
-  Box,
-  Button,
-  FormControl,
-  InputLabel,
-  OutlinedInput,
-  Paper,
-  Typography,
+	Box,
+	Button,
+	FormControl,
+	InputLabel,
+	OutlinedInput,
+	Paper,
+	Typography,
+	useMediaQuery,
 } from "@mui/material";
 
 export default function Newsletter() {
-  return (
-    <Paper
-      elevation={2}
-      sx={{
-        width: "100%",
-        height: "45%",
+	const isSmallScreen = useMediaQuery("(max-width: 428px)");
 
-        mt: "2rem",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          gap: "1.5rem",
-        }}
-      >
-        <Typography
-          variant="h5"
-          textAlign="center"
-          sx={{ pt: "1rem", width: "100%" }}
-        >
-          Suivez notre Newsletter
-        </Typography>
-        {/* <OutlinedInput
+	return (
+		<Paper
+			elevation={2}
+			sx={{
+				width: "100%",
+				height: "45%",
+
+				mt: "2rem",
+			}}
+		>
+			<Box
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					gap: isSmallScreen ? "1rem" : "1.5rem",
+				}}
+			>
+				<Typography
+					variant="h5"
+					textAlign="center"
+					sx={{ pt: "1rem", width: "100%" }}
+				>
+					Suivez notre Newsletter
+				</Typography>
+				<Typography sx={{ pl: "1rem", pr: "1rem" }}>
+					Soyez les premiers à découvrir des stratégies innovantes
+					pour transformer votre entreprise en un modèle d'inclusion
+					et de diversité. Inscrivez-vous dès aujourd'hui et faites
+					partie du changement !"
+				</Typography>
+				{/* <OutlinedInput
           label="Email"
           type="email"
           sx={{ mt: "0.5rem", height: "3rem", width: "80%" }}
         /> */}
-        <FormControl
-          sx={{
-            mt: "0.5rem",
-            height: "3rem",
-            width: "80%",
-          }}
-          variant="outlined"
-        >
-          <InputLabel>Email</InputLabel>
-          <OutlinedInput
-            id="outlined-email"
-            type="email"
-            label="Email"
-            sx={{ "&.MuiOutlinedInput-root": { borderColor: "#00796B" } }}
-          />
-        </FormControl>
+				<FormControl
+					sx={{
+						mt: "0.5rem",
+						height: "3rem",
+						width: "80%",
+					}}
+					variant="outlined"
+				>
+					<InputLabel>Email</InputLabel>
+					<OutlinedInput
+						id="outlined-email"
+						type="email"
+						label="Email"
+						sx={{
+							"&.MuiOutlinedInput-root": {
+								borderColor: "#00796B",
+							},
+						}}
+					/>
+				</FormControl>
 
-        <Button
-          type="submit"
-          sx={{ backgroundColor: "#00796B", color: "white", width: "60%" }}
-        >
-          Envoyer
-        </Button>
-      </Box>
-    </Paper>
-  );
+				<Button
+					type="submit"
+					sx={{
+						backgroundColor: "#00796B",
+						color: "white",
+						width: "60%",
+					}}
+				>
+					Envoyer
+				</Button>
+			</Box>
+		</Paper>
+	);
 }
